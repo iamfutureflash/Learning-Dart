95a6218b6c78e82532db62d3bf2326253f63ca08
iamfutureflash <xenode2000@gmail.com>
2024-03-27 20:45:56 +0530

    variable declaration in dart

--- hello.dart
+++ hello.dart
@@ -1,3 +1,5 @@
 void main() {
-  print('hello dart');
+  print('hello, dart');
+  print('3'*22);
+  print(5/4+2-4*2);
 }
---
+++ variables.dart
@@ -0,0 +1,38 @@
+void main() {
+  // int first = 302;
+  // int second = 51;
+  // print(first);
+  // print(second);
+  // print(first * second);
+  // print(first + second);
+  // print(first - second);
+
+  // double first = 302;
+  // double second = 51;
+  // print(first);
+  // print(second);
+  // print(first * second);
+  // print(first + second);
+  // print(first - second);
+
+  // String name = 'puneet';
+  // print(name);
+
+  // bool name = false;
+  // print(name);
+
+  // dynamic name = 1324;
+  // print(name);
+
+  // String greeting = 'Hello, World';
+  // print(greeting);
+  // greeting = '$greeting , puneet';
+  // print(greeting);
+
+  // var someValue1 = '10';
+  final someValue3 = DateTime.now();
+  const someValue2 = '10';
+  // print(someValue1);
+  print(someValue2);
+  print(someValue3 );
+}
